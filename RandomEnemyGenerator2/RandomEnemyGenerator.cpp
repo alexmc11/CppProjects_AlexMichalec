ca3e83651b61b3f9c9be891f76b0b15ff9a83ab8
alexmichaleccanellas <alexmichaleccanellas@S1-07>
2017-02-16 11:25:20 +0100

    asdasad

--- RandomEnemyGenerator2/RandomEnemyGenerator.cpp
+++ RandomEnemyGenerator2/RandomEnemyGenerator.cpp
@@ -5,6 +5,20 @@ using namespace std;
 
 enum EnemyType {zombie, vampire, ghost, witch};
 
+struct Enemy {
+	EnemyType type;
+	string name;
+	int health;
+};
+
+bool operador(Enemy a, Enemy b)
+{
+	if (a.name == b.name)
+		return true;
+	if (a.type == b.type)
+		return true;
+}
+
 void main() 
 {
 	
